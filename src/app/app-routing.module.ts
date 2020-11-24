46ef6135b9dd80a3ac5f9961ed439653d3415fed
Poojajmpatil <41111770+Poojajmpatil@users.noreply.github.com>
2020-11-24 19:49:26 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { UrlpermissionService } from './urlpermission/urlpermission.service';
+import { SearchComponent } from './search/search.component';
+import { TaxComponent } from './tax/tax.component';
+
 const routes: Routes = [
-  {path:'',component:LoginComponent},
-  {path: 'home',component: HomeComponent,canActivate: [UrlpermissionService]},
-  {path:'login',component:LoginComponent},
+
+  // {path:"",pathMatch:"full",component:SearchComponent},
+  {path:'tax',component:TaxComponent},
 
 
 ];
